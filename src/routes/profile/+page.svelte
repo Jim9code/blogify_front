54e30db0c53fe4a021c8f51f02468ec496bb5727
jeth <rinfwang4@gmail.com>
2024-12-11 22:52:47 +0100

    update

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -2,16 +2,19 @@
 	import { onMount } from "svelte";
   import Profile from "./Profile.svelte";
   import Login from "../Login.svelte";
+	import Spinner from "../Spinner.svelte";
   const serverUrl = import.meta.env.VITE_SERVER_URL
 
 
-
+let loading = false
   let logged_in = false;
   async function login_status() {
+    loading = true
     const res = await fetch(`${serverUrl}/protected`,{credentials:'include'})
     if(res.ok){
       const data = await res.json()
       logged_in = data.authenticated
+      loading = false
     }
   }
 
@@ -25,7 +28,11 @@ onMount(()=>{
   {#if logged_in}
     <Profile/>
       {:else}
-        <Login/>
+      {#if loading}
+        <Spinner/>
+         {:else}
+         <Login/>
+      {/if}
   {/if}
 
 </div>
